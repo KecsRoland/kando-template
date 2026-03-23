592edae756ec948814988f3913479c86fc063761
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2026-03-23 10:18:34 +0100

    Remove BrowserRouter wrapping from App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.jsx'
@@ -7,7 +6,5 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>,
 )
